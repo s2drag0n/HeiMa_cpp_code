458b7413efc78dde87daf8d6b99423dc9be7e0e0
s2drag0n <2558058352@qq.com>
2023-03-30 02:56:45 +0800

    first

--- 黑马cpp_code/024 模板.cpp
+++ 黑马cpp_code/024 模板.cpp
@@ -41,6 +41,28 @@ using namespace std;
 *	template<> boolmyCompare(Person& a, Person& b){}
 * 
 * 学习模板不是为了写模板，而是为了在STL中使用系统提供的模板
+* 
+* 类模板，类中的成员数据类型可以不具体指定，用虚拟的类型表示
+* template<class T1, class T2>
+* class Person{}
+* 
+* 类模板和函数模板区别
+*	1.没有自动类型推导
+*	2.类模板在参数列表中可以有默认参数	template<class T1, class T2 = int>	调用时就无需显式指出第二个数据类型
+* 
+* 类模板中成员函数创建时机
+*	1.普通类中成员函数一开始就可以创建
+*	2.类模板中的成员函数在调用时才能创建，运行时判断是否可以创建：因为在运行前不确定T这个类是否有可以调用的函数
+* 
+* 类模板对象作函数参数
+*	1.指定传入的类型 --- 直接显示对象的数据类型
+*	2.参数模板化 --- 将对象中的参数变为模板进行传递
+*		template<class T1, class T2>
+*		void printPerson2(Person<T1, T2>& p){}
+*		使用typeid(T1).name()可以使用字符串形式查看T1具体是什么类型
+*	3.整个类模板化 --- 将这个对象类型模板化进行传递
+*		template<class T>
+*		void printPerson2(T& p){}
 */
 
 int main() {
